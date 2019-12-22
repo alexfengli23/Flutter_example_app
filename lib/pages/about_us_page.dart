3693e3c733b50ac77bbcf511c9d29f3a8c8bda6b
Alex Feng <jfeng0123@gmail.com>
2019-12-22 10:33:30 -0600

    demo finish

--- lib/pages/about_us_page.dart
+++ lib/pages/about_us_page.dart
@@ -13,7 +13,49 @@ class AboutUsPageState extends State<AboutUsPage>{
   @override
   Widget build(BuildContext context) {
     //Page view
-    return Text('about_us');
+    return Scaffold(
+      body:Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/images/banners/2.jpg',fit: BoxFit.cover,),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('公司介绍'),
+              onTap: (){
+                Navigator.of(context).pushReplacementNamed('company_info');
+              },
+            ),
+            Divider(
+              height: 10.0,
+              color: Colors.grey,
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text('公司优势'),
+              onTap: (){
+
+              },
+            ),
+            Divider(
+              height: 10.0,
+              color: Colors.grey,
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('联系我们'),
+
+            ),
+            Divider(
+              height: 10.0,
+              color: Colors.grey,
+            ),
+          ],
+        ),
+      )
+    );
 
   }
 }
--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,19 +1,70 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import '../model/news.dart';
+import '../services/news.dart';
+import 'news_detail_page.dart';
 
-class NewsPage extends StatefulWidget{
+class NewsPage extends StatefulWidget {
   @override
   NewsPageState createState() => NewsPageState();
 }
 
-class NewsPageState extends State<NewsPage>{
+class NewsPageState extends State<NewsPage> {
+  NewsListModal listData = NewsListModal([]);
 
+  //int page = 0;
+  //获取产品列表并且回调
+  void getNewsList() async {
+    var data = await getNewsResult();
+    NewsListModal list = NewsListModal.fromJson(data);
 
+    setState(() {
+      listData.data.addAll(list.data);
+    });
+  }
+
+  @override
+  void initState() {
+    getNewsList();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     //Page view
-    return Text('news');
+    return Scaffold(
+      //带分隔线的LIST
+      body: ListView.separated(
+          //排列方向
+          scrollDirection: Axis.vertical,
+          //列表项构造器
+          itemBuilder: (BuildContext context, int index) {
+            //新闻列表项数据
+            NewsItemModal item = listData.data[index];
 
+            return ListTile(
+              title: Text(
+                item.title,
+              ),
+              subtitle: Text(item.content),
+              leading: Icon(Icons.fiber_new),
+              trailing: Icon(Icons.arrow_forward),
+              contentPadding: EdgeInsets.all(10.0),
+              enabled: true,
+              onTap: () {
+                Navigator.push(
+                    context,
+                    //传进参数 可查看product_detail_page Constructor的修改
+                    MaterialPageRoute(
+                        builder: (context) => NewsDetailPage(item: item)));
+              },
+            );
+          },
+          //分割线构造器
+          separatorBuilder: (BuildContext context, int index) => Divider(
+                height: 1.0,
+                color: Colors.grey,
+              ),
+          itemCount: listData.data.length),
+    );
   }
 }
